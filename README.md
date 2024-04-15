ad0b2fbc860cd76250b72e3770c964a0e5b579c9
famgz <famgz@proton.me>
2024-04-15 10:15:24 -0300

    readme, srcipts

---
+++ README.md
@@ -0,0 +1,4 @@
+# BroCode MySQL Course Notes
+---
+Notes taken from Bro Code Youtube's MySQL Course at https://www.youtube.com/watch?v=5OdVJbNCSso
+
---
+++ SQL.sql
@@ -0,0 +1,157 @@
+CREATE DATABASE myDB;
+
+USE myDB;
+
+DROP DATABASE myDB;
+
+ALTER DATABASE myDB READ ONLY = 1;
+
+DROP DATABASE myDB; -- cannot cause of the READ ONLY
+
+CREATE TABLE employees (
+        employee_id INT,
+        first_name VARCHAR(50), -- 50: max
+        last_name VARCHAR(50),
+        hourly_pay DECIMAL(5, 2), -- 5: max amount of digits, 2: precision
+        hire_date DATE
+);
+
+SELECT * FROM employees;
+
+RENAME TABLE employess TO workers;
+
+DROP TABLE employees;
+
+ALTER TABLE employees
+ADD phone_number VARCHAR(15);
+
+ALTER TABLE employees
+RENAME COLUMN phone_number to email;
+
+ALTER TABLE employees
+MODIFY COLUMN email VARCHAR(100);
+
+-- move column
+ALTER TABLE employees
+MODIFY email VARCHAR(100)
+AFTER last_name;  -- FIRST: to be first column
+
+-- drop column
+ALTER TABLE employees
+DROP COLUMN email;
+
+-- insert row with all data
+INSET INTO employees
+VALUES (1, "Eugene", "Krabs", 25.50, "2023-01-02");
+
+-- insert multiple rows with all data, will error if any column missing
+INSERT INTO employees
+VALUES 
+    (2, "Squidward", "Tentacles", 15.00, "2023-01-03"),
+    (3, "Spongebob", "Squarepants". 12.50, "2023-01-04"),
+    (4, "Patrick", "Star". 17.50, "2023-01-05"),
+    (5, "Sheldon", "Plankton")  --missing data
+
+-- insert row with missing data
+INSERT INTO employees (employee_id, first_name, last_name)
+VALUES 
+    (6, "Sheldon", "Plankton");
+
+-- select specific columns
+SELECT first_name, last_name
+FROM employees;
+
+-- select with condition
+SELECT *
+FROM employees
+WHERE employee_id = 1;
+
+SELECT *
+FROM employees
+WHERE hourly_pay >= 15;
+
+SELECT *
+FROM employees
+WHERE employee_id != 1;
+
+SELECT *
+FROM employees
+WHERE hire_date IS NOT NULL; -- = NULL doesn`t work
+
+-- update row
+UPDATE employees
+SET hourly_pay = 10.99,
+    hire_date = "2023-01-07"
+WHERE employee_id = 3; -- if not specified will change all rows
+
+-- delete all rows
+DELETE FROM employee; -- will delete all rows in the table!
+
+-- delete row
+DELETE FROM employee
+WHERE employee_id = 4;
+
+
+-- undo changes
+SET AUTOCOMMIT = OFF; -- transaction won't be saved automatically, need to manually save each (COMMIT) transaction
+
+COMMIT; -- create a safe point
+
+DELETE FROM employees;
+
+ROLLBACACK; -- restore to the previous safe point
+
+
+
+--* dates and times
+CREATE TABLE test(
+    my_date DATE,
+    my_time TIME,
+    my_datetime DATETIME,
+);
+
+-- get current dates and times
+INSERT INTO test
+VALUES(CURRENT_DATE(), CURRENT_TIME(), NOW()); -- CURRENT_DATE() + 1 : tomorrow | CURRENT_TIME() - 2000 (seconds)
+
+
+--* constraints
+-- UNIQUE constraint at creation
+CREATE TABLE products (
+    product_id INT,
+    product_name VARCHAR(25) UNIQUE,
+    price DECIMAL(4, 2)
+);
+
+-- UNIQUE constraint afterwards
+ALTER TABLE products
+ADD CONSTRAINT
+UNIQUE(product_name);
+
+-- NOT NULL constraint at creation
+CREATE TABLE products (
+    product_id INT,
+    product_name VARCHAR(25),
+    price DECIMAL(4, 2) NOT NULL
+);
+
+-- NOT NULL constraint afterwards
+ALTER TABLE products
+MODIFY price DECIMAL(4, 2) NOT NULL;
+
+
+-- CHECK constraint at creation
+CREATE TABLE employees (
+        employee_id INT,
+        first_name VARCHAR(50), -- 50: max
+        last_name VARCHAR(50),
+        hourly_pay DECIMAL(5, 2), -- 5: max amount of digits, 2: precision
+        hire_date DATE,
+        --CHECK  (hourly_pay >= 10.00) -- constraint without name
+        CONSTRAINT chk_hourly_pay CHECK (hourly_pay >= 10.00)  -- constraint with a name
+);
+
+-- CHECK constraint afterwards
+ALTER TABLE employees
+ADD CONSTRAINT chk_hourly_pay CHECK(hourly_pay >= 10.00);
+
